89effd4f2733af24b6c9f4bf2bfaaaacd9561bce
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-03 12:56:30 +0100

    Started implementation of IterableWithPolicy (without the policy) (not working currently)

---
+++ 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
@@ -0,0 +1,50 @@
+package it.unibo.inner.test.impl;
+import it.unibo.inner.api.IterableWithPolicy;
+import it.unibo.inner.api.Predicate;
+
+import java.util.Iterator;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
+    private T[] elements;
+
+    public IterableWithPolicyImpl(final T[] e) {
+        this.elements = e;
+    }
+
+    @Override
+    public void setIterationPolicy(Predicate<T> filter) {
+        
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new CustomIterator();
+    }
+
+    public class CustomIterator implements Iterator<T> {
+        private int currentIndex;
+        
+        public CustomIterator() {
+            this.currentIndex = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (this.currentIndex >= 0 && this.currentIndex < IterableWithPolicyImpl.this.elements.length) {
+                return true;
+            }
+
+            else {
+                return false;
+            }
+        }
+
+        public T next() {
+            try {
+                return IterableWithPolicyImpl.this.elements[currentIndex + 1];
+            } finally {
+                this.currentIndex++;
+            }
+        }
+    }
+}
