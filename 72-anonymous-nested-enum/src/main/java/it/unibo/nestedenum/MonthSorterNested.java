b690825c080f65b235e710cb166cce50acb6106a
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-08 15:17:05 +0100

    Added an enum with all months names and a method to check if the month is input is a valide one or not

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -8,6 +8,35 @@ import java.util.Objects;
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
+    private enum Months {
+        january,
+        february,
+        march,
+        april,
+        may,
+        june,
+        july,
+        august,
+        september,
+        october,
+        november,
+        december
+    }
+
+    private void checkMonthName(final String name) {
+        final String normalizedName = name.toLowerCase();
+        boolean isEqual = false;
+        for (final Months month : MonthSorterNested.Months.values()) {
+            if (normalizedName.equals(month.toString())) {
+                isEqual = true;
+                break;
+            }
+        }
+
+        if (!isEqual) {
+            throw new IllegalArgumentException("The months " + name + " does not exists");
+        }
+    }
 
     @Override
     public Comparator<String> sortByDays() {
