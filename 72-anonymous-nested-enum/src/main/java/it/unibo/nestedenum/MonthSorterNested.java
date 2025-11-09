f5f92155c23db22b61d5b743559eb0f3ac5b47de
Giacomo Fantini <giacomo.fantini5@studio.unibo.it>
2025-11-09 12:17:16 +0100

    Fixed implementation of 'sortByOrder()': now it correclty compares what month is before the other one by using it's numerical representation, not alphabetical order

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -32,7 +32,7 @@ public final class MonthSorterNested implements MonthSorter {
                 checkMonthName(s1);
                 checkMonthName(s2);
 
-                return Integer.compare(Month.valueOf(s1.toUpperCase()).getValue(), Month.valueOf(s2.toUpperCase()).getValue());
+                return 0; // To Re-implement
             }
         };
     }
@@ -45,7 +45,7 @@ public final class MonthSorterNested implements MonthSorter {
                 checkMonthName(s1);
                 checkMonthName(s2);
 
-                return s1.compareTo(s2);
+                return Integer.compare(Month.valueOf(s1.toUpperCase()).getValue(), Month.valueOf(s2.toUpperCase()).getValue());
             }
         };
     }
